f25d44d3ee3f9e59b9b969ea09033ecec3901e45
Maria Fernanda Valencia Botello <mfvalenciab@correo.iue.edu.co>
2024-10-11 11:47:45 -0500

    creo array con doctores, conecto alldoctor con infodoctor y diseño de alldoctor

---
+++ proyecto_movil_front/src/screens/InfoDoctor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native'; // Agregar ScrollView
+
+const InfoDoctorScreen = () => {
+  return (
+    <View>
+      <Text>Hola, estoy en info de doctor</Text>
+    </View>
+  );
+};
+
+export default InfoDoctorScreen;
