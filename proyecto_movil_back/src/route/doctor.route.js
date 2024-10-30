916cffebec2ed407bbd0d527609d46a465b51010
Maria Fernanda Valencia Botello <mfvalenciab@correo.iue.edu.co>
2024-10-29 20:29:32 -0500

    back del infodoctor agregado y terminado

--- proyecto_movil_back/src/route/doctor.route.js
+++ proyecto_movil_back/src/route/doctor.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { metodosDoctor } from '../controller/doctor.controller.js';
+import { infoDoctor } from '../controller/infodoctor.controller.js';
 
 const router = Router();
 
 router.get('/medicos', metodosDoctor.getDoctor);
+router.get('/medicos/:id', infoDoctor.getInfoDoctor);
 
 export default router;
