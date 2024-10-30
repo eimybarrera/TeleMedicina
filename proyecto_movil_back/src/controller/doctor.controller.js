9a44e72ab4685d7953f217565b29c33faf8208d5
Maria Fernanda Valencia Botello <mfvalenciab@correo.iue.edu.co>
2024-10-29 20:03:58 -0500

    back alldoctor terminado

--- proyecto_movil_back/src/controller/doctor.controller.js
+++ proyecto_movil_back/src/controller/doctor.controller.js
@@ -3,7 +3,14 @@ import getConnection from '../database/database.js';
 const getDoctor = async (req, res) => {
   try {
     const connection = await getConnection();
-    const [result] = await connection.query('SELECT * FROM medicos');
+    const [result] = await connection.query(`
+      SELECT id_medico AS id, nombre AS name, e.nombre_especialidad AS specialty, 
+              c.nombre_centro AS medicalCenter, calificacion AS rating, reseñas AS review, 
+              foto_perfil AS imageUrl
+      FROM medicos m
+      LEFT JOIN especialidades e ON m.id_especialidad = e.id_especialidad
+      LEFT JOIN centros_medicos c ON m.id_centro = c.id_centro
+    `);
     console.log(result);
     res.json(result);
   } catch (error) {
