8c5d97f6081fc62e3a6c92b9e63078060faeff51
Maria Fernanda Valencia Botello <mfvalenciab@correo.iue.edu.co>
2024-11-07 18:32:29 -0500

    Se agrega cors al back y se conecta el screen alldoctor con el back

--- proyecto_movil_back/src/app.js
+++ proyecto_movil_back/src/app.js
@@ -1,8 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import centerrouter from './route/Centrosmedicos.route.js';
 import router from './route/doctor.route.js';
 import patientRouter from './route/patient.route.js';
+
 const app = express();
 
 //configuracion servidor/instancia
@@ -11,6 +13,14 @@ app.set('port', process.env.PORT || 3000); //puerto, puerto por defecto y numero
 //middleware
 app.use(morgan('dev'));
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:8081',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 //rutas
 app.use('/', router);
