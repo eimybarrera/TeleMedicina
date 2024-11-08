825c02cfd3596e0aebb295c05c3215ab87f4968c
Maria Fernanda Valencia Botello <mfvalenciab@correo.iue.edu.co>
2024-11-07 20:26:09 -0500

    se agrega endpoint para reseñas

--- proyecto_movil_back/src/route/doctor.route.js
+++ proyecto_movil_back/src/route/doctor.route.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/medicos', metodosDoctor.getDoctor);
 router.get('/medicos/:id', metodosDoctor.getInfoDoctor);
+router.get('/medicos/:id/resena', metodosDoctor.getDoctorReviews);
 router.get('/medicos/:id/fechas-disponibles', metodosDoctor.getFechasDisponibles);
 router.get('/medicos/:doctorId/fechas-disponibles/:fechaId/horas-disponibles', metodosDoctor.getHorasDisponibles);
 
